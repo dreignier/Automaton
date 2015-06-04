f3f1df02aaed6f760cec22d05b8ea7cbc6eeb8a0
David Reignier <david.reignier@gmail.com>
2015-06-04 21:49:59 +0200

    Fix the encoding of the logs via web

--- commands/logs.js
+++ commands/logs.js
@@ -101,7 +101,7 @@ module.exports = function(bot) {
                   return;
                }
 
-               res.writeHead(200, {'Content-Type': 'text/plain'});
+               res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
                res.end(data);
             });
          }
