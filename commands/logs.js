91d46175e9d85e654fa0d7f887684f8928ac75cd
David Reignier <david.reignier@gmail.com>
2015-06-04 21:02:02 +0200

    improve logs

--- commands/logs.js
+++ commands/logs.js
@@ -12,11 +12,62 @@ module.exports = function(bot) {
    }
 
    var getLogPath = function(to) {
-      return directory + to.replace('#', '') + moment().format('YYYY-MM-DD') + '.log';
+      return directory + to.replace('#', '') + '-' + moment().format('YYYY-MM-DD') + '.log';
    };
 
-   bot.client.addListener('message', function(from, to, text, message) {
-      fs.appendFileSync(getLogPath(to), '(' + moment().format('HH:mm:ss') + ') ' + from + ' : ' + text + '\n');
+   var log = function(channel, text) {
+      fs.appendFileSync(getLogPath(channel), '(' + moment().format('HH:mm:ss') + ') ' + text + '\n');
+   }
+
+   bot.client.addListener('message', function(from, to, text) {
+      log(to, from + ' : ' + text);
+   });
+
+   bot.client.addListener('nick', function(old, nick, channels) {
+      log(channels[0], '* NICK ' + old + ' => ' + nick);
+   });
+
+   bot.client.addListener('join', function(to, nick) {
+      log(to, '* JOIN ' + nick);
+   });
+
+   bot.client.addListener('quit', function() {
+      var message = '* QUIT ' + nick;
+
+      if (reason) {
+         message += ' : ' + reason;
+      }
+
+      log(to, message);
+   });   
+
+   bot.client.addListener('part', function(to, nick, reason) {
+      var message = '* PART ' + nick;
+
+      if (reason) {
+         message += ' : ' + reason;
+      }
+
+      log(to, message);
+   });
+
+   bot.client.addListener('kick', function(to, nick, kicker, reason) {
+      var message = '* KICK ' + nick + ' kicked by ' + kicker;
+
+      if (reason) {
+         message += ' : ' + reason;
+      }
+
+      log(to, message);
    });
    
+   bot.client.addListener('selfMessage', function(to, text) {
+      // Never log messages to Q
+      if (to.toLowerCase() == 'q@cserve.quakenet.org') {
+         return;
+      }
+
+      log(to, bot.options.nick + ' : ' + text);
+   });
+
 };
